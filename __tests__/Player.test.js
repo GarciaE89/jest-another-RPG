f42e284cea087289662c157c7a93f9036b55b045
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-10-09 22:00:20 -0600

    added player invetory and health tests/constructors

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,9 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion')
 console.log(new Potion());
 const Player = require('../lib/Player');
+const { TestWatcher } = require('@jest/core');
+
+
 
 
 
@@ -53,4 +56,26 @@ test("subtract from player's health", () => {
     expect(player.health).toBe(oldHealth - 5);
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
+});
+
+test("gets player attack value", () => {
+const player = new Player('Dave');
+player.strength = 10;
+expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+    player.usePotion(1); 
+    expect(player.inventory.length).toBeLessThan(oldCount);
 });
